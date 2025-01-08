dc20a480400003a6b938f17af6f7af49a77f89b3
Ademola Madelewi <ademolamadelewi@gmail.com>
2025-01-08 14:05:58 +0100

    Update featured.js

--- js/featured.js
+++ js/featured.js
@@ -2,25 +2,32 @@ const featuredContainer = document.querySelector(".featured-container");
 
 const projects = [
   {
-    link: "https://hertechchoice.com/",
+    link: "https://deelaa.travel/",
     imageLink:
-      "https://res.cloudinary.com/ademolamadelewi/image/upload/v1671636406/1-Mockup_ae4w6k.png",
-    title: "Hertechchoice",
-    desc: "Her Tech Choice is a 6-Weeks virtual bootcamp for ladies who are interested in software development. It is an avenue to network and collaborate with other participants with the same drive and vision.",
+      "https://res.cloudinary.com/ademolamadelewi/image/upload/q_auto:low/v1736340937/Group_7_owhre4.webp",
+    title: "Deelaa Travel",
+    desc: "A web application for seamless flight booking, offering users the ability to search, compare, and book flights online with ease.",
   },
   {
-    link: "https://www.goldbar.games/",
+    link: "https://instantplay.goldbar.games/#/login",
     imageLink:
-      "https://res.cloudinary.com/ademolamadelewi/image/upload/v1671636401/03-Mockup_etovko.png",
+      "https://res.cloudinary.com/ademolamadelewi/image/upload/q_auto:low/03-Mockup_etovko.webp",
     title: "Goldbar Games",
     desc: "A revenue sharing NFT collection with a decentralised gaming platform on the solana blockchain",
   },
   {
     link: "https://elfrique.com",
     imageLink:
-      "https://res.cloudinary.com/ademolamadelewi/image/upload/v1671636400/04-Mockup_akt2nf.png",
+      "https://res.cloudinary.com/ademolamadelewi/image/upload/q_auto:low/04-Mockup_akt2nf.webp",
     title: "Elfrique",
     desc: "Elfrique is an innovative and complete goal-oriented company, which is into event management services such as event ticketing, voting contest management, trivia management system, event vendor management system, event forms management system and travel & tours management services.",
+  },
+   {
+    link: "https://hertechchoice.com/",
+    imageLink:
+      "https://res.cloudinary.com/ademolamadelewi/image/upload/q_auto:low/1-Mockup_ae4w6k.webp",
+    title: "Hertechchoice",
+    desc: "Her Tech Choice is a 6-Weeks virtual bootcamp for ladies who are interested in software development. It is an avenue to network and collaborate with other participants with the same drive and vision.",
   },
 ];
 
